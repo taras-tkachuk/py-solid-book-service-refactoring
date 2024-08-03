ed13d6fea67922edd52a37049209f146134a9fcb
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-08-03 17:19:00 +0300

    Solution

---
+++ app/book.py
@@ -0,0 +1,12 @@
+class Book:
+    def __init__(self, title: str, content: str) -> None:
+        self._title = title
+        self._content = content
+
+    @property
+    def title(self) -> str:
+        return self._title
+
+    @property
+    def content(self) -> str:
+        return self._content
---
+++ app/serialize.py
@@ -0,0 +1,24 @@
+import json
+import xml.etree.ElementTree as ET
+from abc import ABC, abstractmethod
+
+
+class SerializeStrategy(ABC):
+    @abstractmethod
+    def serialize(self, title: str, content: str) -> str:
+        pass
+
+
+class JSONSerializeStrategy(SerializeStrategy):
+    def serialize(self, title: str, content: str) -> str:
+        return json.dumps({"title": title, "content": content})
+
+
+class XMLSerializeStrategy(SerializeStrategy):
+    def serialize(self, title: str, content: str) -> str:
+        root = ET.Element("book")
+        title_elem = ET.SubElement(root, "title")
+        title_elem.text = title
+        content_elem = ET.SubElement(root, "content")
+        content_elem.text = content
+        return ET.tostring(root, encoding="unicode")
